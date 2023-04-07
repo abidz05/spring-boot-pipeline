2a8426ffcec58773d87611c97dcc20cf377738d7
Abid Abuzaid <abidz05@gmail.com>
2023-04-07 15:21:03 -0400

    initial release

---
+++ src/test/java/com/springboottest/springbootpipeline/SpringBootPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboottest.springbootpipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
